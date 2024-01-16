b8a6ff7c850423dc156efbc4fdd3b02489891377
Niedzwiedz1 <niewadzilukasz@gmail.com>
2024-01-16 15:48:46 +0000

    Add function to filter data according to user input

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
+days = st.slider("Forecast Days", min_value=1, max_value=5,
+                 help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for next {days} days in {place}")
 
 
-def get_data(days):
-    dates = ["2020", "2021", "2022"]
-    temperatures = [10, 15, 35]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+
 
 
 d, t = get_data(days)
